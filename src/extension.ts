917de547fcd17a97510a80ca32186448a83b8c7c
qcp38 <felix.langbein@uni-ulm.de>
2023-05-16 16:50:31 +0200

    added new commands and set up communication

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as fs from 'fs';
+import ReactPanel from './reactPanel';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -14,115 +16,75 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('aldesco-extension.visualizer', () => {
-		ReactPanel.createOrShow(context.extensionPath);
+
+	let reactPanel: ReactPanel | undefined;
+	let basename: string;
+	let fileContent: string;
+
+	//Opens Visualizer without json
+	let disposableVis = vscode.commands.registerCommand('aldesco-extension.visualizer', () => {
+		reactPanel = ReactPanel.createOrShow(context.extensionPath);
 	});
 
-	context.subscriptions.push(disposable);
+	//opens Visualizer with json (right click on json file)
+	let disposableVisWithJson = vscode.commands.registerCommand('aldesco-extension.openVisWithJson', (fileUri: vscode.Uri) => {
+		basename = path.posix.basename(fileUri.path);
+		fileContent = fs.readFileSync(fileUri.fsPath, 'utf8');
+
+		reactPanel = ReactPanel.createOrShow(context.extensionPath);
+
+		if(reactPanel && basename && fileContent){
+			console.log('opening json');
+			reactPanel.sendMessage('VSCtest', {name: basename, content: fileContent});
+		}
+	});
+
+	//connect to visualizer via url (locally start visualizer first)
+	let disposableVisLH = vscode.commands.registerCommand('aldesco-extension.connectToVisLH', () => {
+		reactPanel = ReactPanel.createOrShowLH(context.extensionPath);
+	});
+
+
+	//open json file in Visualizer (if open)
+	let disposabelOpenJsonInVis = vscode.commands.registerCommand('aldesco-extension.openJsonInVis', () => {
+
+		vscode.window.showOpenDialog({
+			canSelectFiles: true,
+			canSelectFolders: false,
+			canSelectMany: false,
+			filters: {
+			  'JsonFiles': ['json', '\\json(-\d+)?$'],
+			  'AllFiles': ['*']
+			}
+		  }).then(fileUris => {
+			if (fileUris && fileUris[0]) {
+			  const fileUri = fileUris[0];
+		
+			  // Read the content of the selected JSON file
+			  vscode.workspace.fs.readFile(fileUri).then(content => {
+				const jsonContent = content.toString();
+				
+				reactPanel = ReactPanel.createOrShow(context.extensionPath);
+				// Pass the file name and content to the webview
+				reactPanel.sendMessage('VSCtest', { name: fileUri.fsPath, content: jsonContent });
+			  });
+			}
+		  });
+
+		// if(reactPanel && basename && fileContent){
+		// 	console.log('message send');
+		// 	reactPanel.sendMessage('VSCtest', {name: basename, content: fileContent});
+		// }
+	});
+
+	context.subscriptions.push(disposableVisLH);
+	context.subscriptions.push(disposabelOpenJsonInVis);
+	context.subscriptions.push(disposableVisWithJson);
+	context.subscriptions.push(disposableVis);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
-
-/**
- * Manages react webview panels
- */
-class ReactPanel {
-	/**
-	 * Track the currently panel. Only allow a single panel to exist at a time.
-	 */
-	public static currentPanel: ReactPanel | undefined;
-
-	private static readonly viewType = 'react';
-
-	private readonly _panel: vscode.WebviewPanel;
-	private readonly _extensionPath: string;
-	private _disposables: vscode.Disposable[] = [];
-
-	public static createOrShow(extensionPath: string) {
-		const column = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
-
-		// If we already have a panel, show it.
-		// Otherwise, create a new panel.
-		if (ReactPanel.currentPanel) {
-			ReactPanel.currentPanel._panel.reveal(column);
-		} else {
-			ReactPanel.currentPanel = new ReactPanel(extensionPath, column || vscode.ViewColumn.One);
-		}
-	}
-
-	private constructor(extensionPath: string, column: vscode.ViewColumn) {
-		this._extensionPath = extensionPath;
-
-		// Create and show a new webview panel
-		this._panel = vscode.window.createWebviewPanel(ReactPanel.viewType, "React", column, {
-			// Enable javascript in the webview
-			enableScripts: true,
-
-			// And restric the webview to only loading content from our extension's `media` directory.
-			localResourceRoots: [
-				vscode.Uri.file(path.join(this._extensionPath, 'visualizer_dist'))
-			]
-		});
-		
-		// Set the webview's initial html content 
-		this._panel.webview.html = this._getHtmlForWebview();
-
-		// Listen for when the panel is disposed
-		// This happens when the user closes the panel or when the panel is closed programatically
-		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
-
-		// Handle messages from the webview
-		this._panel.webview.onDidReceiveMessage(message => {
-			switch (message.command) {
-				case 'alert':
-					vscode.window.showErrorMessage(message.text);
-					return;
-			}
-		}, null, this._disposables);
-	}
-
-	public doRefactor() {
-		// Send a message to the webview webview.
-		// You can send any JSON serializable data.
-		this._panel.webview.postMessage({ command: 'refactor' });
-	}
-
-	public dispose() {
-		ReactPanel.currentPanel = undefined;
-
-		// Clean up our resources
-		this._panel.dispose();
-
-		while (this._disposables.length) {
-			const x = this._disposables.pop();
-			if (x) {
-				x.dispose();
-			}
-		}
-	}
-
-	private _getHtmlForWebview() {
-		const manifest = require(path.join(this._extensionPath, 'visualizer_dist', 'asset-manifest.json'));
-		const mainScript = manifest['files']['main.js'];
-		const icon = manifest['files']['favicon.png'];
-
-		const scriptPathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'visualizer_dist', mainScript));
-		const iconPathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'visualizer_dist', icon));
-
-		const scriptUri = this._panel.webview.asWebviewUri(scriptPathOnDisk);
-		const iconUri = this._panel.webview.asWebviewUri(iconPathOnDisk);
-
-		return `<!doctype html>
-		<html>
-		<head>
-		<meta charset="utf-8"><title>AST Prototype Visualizer</title><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="${iconUri}">
-		<script defer="defer" src="${scriptUri}"></script>
-		</head>
-		<body></body>
-		</html>`;
-	}
-}
 
 function getNonce() {
 	let text = "";
