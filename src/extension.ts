38475a4b4d73404670087d41eb039cf92ee39319
qcp38 <felix.langbein@uni-ulm.de>
2023-05-29 12:42:10 +0200

    added/updated commands

--- src/extension.ts
+++ src/extension.ts
@@ -22,83 +22,114 @@ export function activate(context: vscode.ExtensionContext) {
 	let fileContent: string;
 
 	//Opens Visualizer without json
-	let disposableVis = vscode.commands.registerCommand('aldesco-extension.visualizer', () => {
-		reactPanel = ReactPanel.createOrShow(context.extensionPath);
-	});
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.visualizer', () => {
+			reactPanel = ReactPanel.createOrShow(context.extensionPath);
+			updateReactPanel(reactPanel);
+		})
+	);
 
 	//opens Visualizer with json (right click on json file)
-	let disposableVisWithJson = vscode.commands.registerCommand('aldesco-extension.rightClickLogFile', (fileUri: vscode.Uri) => {
-		basename = path.posix.basename(fileUri.path);
-		fileContent = fs.readFileSync(fileUri.fsPath, 'utf8');
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.rightClickLogFile', (fileUri: vscode.Uri, tree?: string) => {
+			basename = path.posix.basename(fileUri.path);
+			fileContent = fs.readFileSync(fileUri.fsPath, 'utf8').toString();
 
-		reactPanel = ReactPanel.createOrShow(context.extensionPath);
+			reactPanel = ReactPanel.createOrShow(context.extensionPath);
 
-		if (reactPanel && basename && fileContent) {
-			console.log('opening json');
-			reactPanel.sendMessage('VSC:OpenFile', { name: basename, content: fileContent });
-		}
-	});
-
-	//connect to visualizer via url (locally start visualizer first) (mainly for easier debugging)
-	let disposableVisLH = vscode.commands.registerCommand('aldesco-extension.connectToVisLH', () => {
-		reactPanel = ReactPanel.createOrShowLH(context.extensionPath);
-	});
-
+			if (reactPanel && basename && fileContent) {
+				reactPanel.sendMessage('VSC:OpenFile', { name: basename, content: fileContent, tree: tree ? tree : '/' });
+			}
+			updateReactPanel(reactPanel);
+		})
+	);
 
 	//open json file in Visualizer
-	let disposabelOpenJsonInVis = vscode.commands.registerCommand('aldesco-extension.openJsonInVis', () => {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.openJsonInVis', () => {
+			const activeEditor = vscode.window.activeTextEditor;
 
-		const activeEditor = vscode.window.activeTextEditor;
+			if (!activeEditor || !activeEditor.document.fileName.includes('vis')) {
+				//TODO: only be able to select log files
+				vscode.window.showOpenDialog({
+					canSelectFiles: true,
+					canSelectFolders: false,
+					canSelectMany: false,
+					filters: {
+						'AllFiles': ['*']
+					}
+				}).then(fileUris => {
+					if (fileUris && fileUris[0]) {
+						const fileUri = fileUris[0];
 
-		if (!activeEditor || !activeEditor.document.fileName.includes('vis')) {
-			//TODO: only be able to select log files
-			vscode.window.showOpenDialog({
-				canSelectFiles: true,
-				canSelectFolders: false,
-				canSelectMany: false,
-				filters: {
-					'AllFiles': ['*']
-				}
-			}).then(fileUris => {
-				if (fileUris && fileUris[0]) {
-					const fileUri = fileUris[0];
+						// Read the content of the selected JSON file
+						vscode.workspace.fs.readFile(fileUri).then(content => {
+							const jsonContent = content.toString();
 
-					// Read the content of the selected JSON file
-					vscode.workspace.fs.readFile(fileUri).then(content => {
-						const jsonContent = content.toString();
+							reactPanel = ReactPanel.createOrShow(context.extensionPath);
+							// Pass the file name and content to the webview
+							reactPanel.sendMessage('VSC:OpenFile', { name: fileUri.fsPath, content: jsonContent });
+						});
+					}
+				});
+			} else {
+				const fileUri = activeEditor.document.uri;
+				vscode.workspace.fs.readFile(fileUri).then(content => {
+					const jsonContent = content.toString();
 
-						reactPanel = ReactPanel.createOrShow(context.extensionPath);
-						// Pass the file name and content to the webview
-						reactPanel.sendMessage('VSC:OpenFile', { name: fileUri.fsPath, content: jsonContent });
-					});
-				}
-			});
-		} else {
-			const fileUri = activeEditor.document.uri;
-			vscode.workspace.fs.readFile(fileUri).then(content => {
-				const jsonContent = content.toString();
+					reactPanel = ReactPanel.createOrShow(context.extensionPath);
+					// Pass the file name and content to the webview
+					reactPanel.sendMessage('VSC:OpenFile', { name: fileUri.fsPath, content: jsonContent });
+				});
+			}
+			updateReactPanel(reactPanel);
+		})
+	);
 
-				reactPanel = ReactPanel.createOrShow(context.extensionPath);
-				// Pass the file name and content to the webview
-				reactPanel.sendMessage('VSC:OpenFile', { name: fileUri.fsPath, content: jsonContent });
-			});
-		}
-	});
+	//for duplicating the last used webview
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.duplicateVis', () => {
+			if (reactPanel) {
+				reactPanel.duplicateActive(context.extensionPath);
+			} else {
+				vscode.window.showInformationMessage('No Visualizer Active to Duplicate');
+			}
+		})
+	);
 
-	context.subscriptions.push(disposableVisLH);
-	context.subscriptions.push(disposabelOpenJsonInVis);
-	context.subscriptions.push(disposableVisWithJson);
-	context.subscriptions.push(disposableVis);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.openPattern', (fileUri: vscode.Uri) => {
+			vscode.commands.executeCommand('aldesco-extension.rightClickLogFile', fileUri, '/patternView');
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.openAST', (fileUri: vscode.Uri) => {
+			vscode.commands.executeCommand('aldesco-extension.rightClickLogFile', fileUri, '/ASTView');
+		})
+	);
+
+
+	//connect to visualizer via url (locally start visualizer first) (mainly for easier debugging)
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.connectToVisLH', () => {
+			reactPanel = ReactPanel.createOrShowLH(context.extensionPath);
+			updateReactPanel(reactPanel);
+		})
+	);
+
+	//testing
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.sendMessage', () => {
+			reactPanel?.sendMessage("test", "heeeeelllo");
+		})
+	);
+
+}
+function updateReactPanel(reactPanel: ReactPanel | undefined) {
+	vscode.commands.executeCommand('setContext', 'reactPanel', reactPanel);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
 
-function getNonce() {
-	let text = "";
-	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
-}
