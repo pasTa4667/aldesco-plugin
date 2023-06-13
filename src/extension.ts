c56d2f3d544bfa5ca007184970f706f1afecbacc
qcp38 <felix.langbein@uni-ulm.de>
2023-06-13 13:36:27 +0200

    refactoring/ new commands

--- src/extension.ts
+++ src/extension.ts
@@ -5,38 +5,45 @@ import * as path from 'path';
 import * as fs from 'fs';
 import ReactPanel from './reactPanel';
 import Prototype from './prototype';
+import { forEach } from 'underscore';
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Your extension is activated once vscode has finished starting up
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('extension "aldesco-extension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
+	// The commands have been defined in the package.json file
 	// The commandId parameter must match the command field in package.json
 	const configuration = vscode.workspace.getConfiguration('aldesco-extension');
+	
+	//get active editor and enable/disable visualizeSpoonAST command
+	const activeEditor = vscode.window.activeTextEditor;
+	updateIsEditorJava(activeEditor);
+
 	let reactPanel: ReactPanel | undefined;
 	let basename: string;
 	let fileContent: string;
 
 	//Opens Visualizer without log File
 	context.subscriptions.push(
-		vscode.commands.registerCommand('aldesco-extension.visualizer', () => {
-			reactPanel = ReactPanel.createOrShow(context.extensionPath);
+		vscode.commands.registerCommand('aldesco-extension.visualizer', async () => {
+			reactPanel = await ReactPanel.createOrShow(context.extensionPath);
+			//opens the last used vis file
+			reactPanel.sendMessage('VSC:OpenFile' , {name: '', content: '', tree: '/'});
 			updateReactPanel(reactPanel);
 		})
 	);
 
 	//opens Visualizer with log file (right click on json file)
 	context.subscriptions.push(
-		vscode.commands.registerCommand('aldesco-extension.rightClickLogFile', (fileUri: vscode.Uri, tree?: string) => {
+		vscode.commands.registerCommand('aldesco-extension.rightClickLogFile', async (fileUri: vscode.Uri, tree?: string) => {
 			basename = path.posix.basename(fileUri.path);
 			fileContent = fs.readFileSync(fileUri.fsPath, 'utf8').toString();
 
-			reactPanel = ReactPanel.createOrShow(context.extensionPath, basename);
+			reactPanel = await ReactPanel.createOrShow(context.extensionPath, basename);
 
 			if (reactPanel && basename && fileContent) {
 				if (tree !== '/' && tree !== '/ASTView' && tree !== '/patternView') {
@@ -52,8 +59,6 @@ export function activate(context: vscode.ExtensionContext) {
 	//open Log file in Visualizer
 	context.subscriptions.push(
 		vscode.commands.registerCommand('aldesco-extension.openLogFileInVis', () => {
-			const activeEditor = vscode.window.activeTextEditor;
-
 			if (!activeEditor || !activeEditor.document.fileName.includes('vis')) {
 				//TODO: only be able to select log files
 				vscode.window.showOpenDialog({
@@ -91,8 +96,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	//for opening the most recently generated visualizer log file
 	context.subscriptions.push(
-		vscode.commands.registerCommand('aldesco-extension.openMostRecentLogFile', () => {
-			const logFileDirPath = configuration.get('prototype.visualizerLogFileDirectoryLocation') as string;
+		vscode.commands.registerCommand('aldesco-extension.openMostRecentLogFile', (tree?: string) => {
+			const aldescoProjectDir = configuration.get('prototype.aldescoProjectDirectory') as string;
+			const logFileDirPath = path.join(aldescoProjectDir, '.visualizer-logs');
+			console.log(logFileDirPath);
 
 			const directories = fs.readdirSync(logFileDirPath, { withFileTypes: true })
 				.filter(dirent => dirent.isDirectory())
@@ -125,7 +132,14 @@ export function activate(context: vscode.ExtensionContext) {
 					const latestFile = files[0];
 					const latestFilePath = path.join(latestDirPath, latestFile);
 
-					readFileOpenVis(reactPanel, vscode.Uri.file(latestFilePath), context.extensionPath);
+					if(!latestFilePath.includes('vis')){
+						vscode.window.showInformationMessage('No Vis File found to display');
+						return;
+					}
+					if (tree !== '/' && tree !== '/ASTView' && tree !== '/patternView') {
+						tree = '/';
+					}
+					readFileOpenVis(reactPanel, vscode.Uri.file(latestFilePath), context.extensionPath, tree);
 				}
 			}
 		})
@@ -144,6 +158,30 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 
 
+	
+	//start prototype folder matching
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.startPrototype', () => {
+			Prototype.execute(context.extensionPath);
+		})
+	);
+	
+	//start prototype spoon ast visualizer with method or file
+	context.subscriptions.push(
+		vscode.commands.registerCommand('aldesco-extension.visualizeSpoonAST', async (...args) => {
+
+			if(activeEditor){
+				const fileUri = activeEditor.document.uri;
+				const startLine = activeEditor.selection.start;
+
+				//startLine + 1 since vscode gives us the line before the selected line 
+				if (await Prototype.visualizeSpoonAST(context.extensionPath, fileUri.fsPath, startLine && args.length > 0 ? startLine.line + 1: undefined)){
+					vscode.commands.executeCommand('aldesco-extension.openMostRecentLogFile', '/ASTView');
+				}				
+			}
+		})
+	);
+		
 	//connect to visualizer via url (locally start visualizer first) (mainly for easier debugging)
 	context.subscriptions.push(
 		vscode.commands.registerCommand('aldesco-extension.connectToVisLH', () => {
@@ -159,25 +197,31 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	);
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('aldesco-extension.startPrototype', () => {
-			const prototype = new Prototype(context.extensionPath);
-			prototype.execute();
-		})
-	);
+	//checking whether active editor is java file, if so enable command
+	vscode.window.onDidChangeActiveTextEditor((editor) => {
+		updateIsEditorJava(editor);
+	});
 
+}
+
+function updateIsEditorJava(editor: vscode.TextEditor | undefined){
+	if (editor && editor.document.uri.fsPath.includes('.java')) {
+		vscode.commands.executeCommand('setContext', 'isEditorJava', true);
+	}else{
+		vscode.commands.executeCommand('setContext', 'isEditorJava', false);
+	}
 }
 
 function updateReactPanel(reactPanel: ReactPanel | undefined) {
 	vscode.commands.executeCommand('setContext', 'reactPanel', reactPanel);
 }
 
-function readFileOpenVis(reactPanel: ReactPanel | undefined, fileUri: vscode.Uri, extensionPath: string) {
-	vscode.workspace.fs.readFile(fileUri).then(content => {
+function readFileOpenVis(reactPanel: ReactPanel | undefined, fileUri: vscode.Uri, extensionPath: string, tree?: string) {
+	vscode.workspace.fs.readFile(fileUri).then(async content => {
 		const jsonContent = content.toString();
-		reactPanel = ReactPanel.createOrShow(extensionPath, fileUri.fsPath);
+		reactPanel = await ReactPanel.createOrShow(extensionPath, fileUri.fsPath);
 		// Pass the file name and content to the webview
-		reactPanel.sendMessage('VSC:OpenFile', { name: fileUri.fsPath, content: jsonContent });
+		reactPanel.sendMessage('VSC:OpenFile', { name: fileUri.fsPath, content: jsonContent, tree: tree? tree : '/' });
 	});
 }
 
