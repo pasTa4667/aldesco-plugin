35da2ed70ad7994bcf7eeec458515d4334e885d3
qcp38 <felix.langbein@uni-ulm.de>
2023-06-13 13:36:47 +0200

    added visualizeSpoonAST function

--- src/prototype.ts
+++ src/prototype.ts
@@ -4,23 +4,48 @@ import * as vscode from 'vscode';
 
 export default class Prototype {
 
-    private readonly _extensionPath: string;
-    private readonly _prototypePath = 'prototype/ast-prototype-1.0.0.jar';
-    private readonly _configuration = vscode.workspace.getConfiguration('aldesco-extension');
+    private static readonly _prototypePath = 'prototype/ast-prototype-1.0.0.jar';
+    private static readonly _configuration = vscode.workspace.getConfiguration('aldesco-extension');
+    private static readonly _aldescoProjectDir = this._configuration.get('prototype.aldescoProjectDirectory') as string;
 
-    constructor(extensionPath: string) {
-        this._extensionPath = extensionPath;
+    public static visualizeSpoonAST(extensionPath: string, file: string, startLine?: number): Promise<boolean>{
+        return new Promise((resolve) => {
+            const args = ['--file', file];
+            const prototypeJarLocation = path.join(this._aldescoProjectDir, 'build', 'libs', 'ast-prototype-1.0.0.jar');
+            
+            if(startLine){
+                args.push('--line');
+                args.push(startLine.toString());
+            }
+            
+            const childProcess = spawn('java', ['-jar', prototypeJarLocation, ...args], { cwd: this._aldescoProjectDir });
+    
+            // Handle events and output from the child process
+            childProcess.stdout.on('data', (data) => {
+               vscode.window.showInformationMessage(`Vsualizer: ${data}`);
+            });
+    
+            childProcess.stderr.on('data', (data) => {
+                vscode.window.showErrorMessage(`Visualizer file could not be generated:\n ${data}`);
+                resolve(false);
+            });
+    
+            childProcess.on('close', (code) => {
+                code === 0 ? resolve(true) : resolve(false);
+                console.log(`child process exited with code ${code}`);
+            });
+        });
     }
 
-    public execute(...args: string[]) {
-        const prototype = path.join(this._extensionPath, 'prototype', 'ast-prototype-1.0.0.jar');
-        const testFolder = path.join(this._extensionPath, 'prototype', 'files', '12');
-        const chain = path.join(this._extensionPath, 'prototype', 'chains', 'FindAllMethods.class');
+    public static execute(extensionPath: string, ...args: string[]) {
+        const prototype = path.join(extensionPath, 'prototype', 'ast-prototype-1.0.0.jar');
+        const testFolder = path.join(extensionPath, 'prototype', 'files', '12');
+        const chain = path.join(extensionPath, 'prototype', 'chains', 'FindAllMethods.class');
 
         const outputLocation = this._configuration.get('prototype.outputLocation') as string;
         const chainLocation = this._configuration.get('prototype.chainLocation') as string;
         let inputLocation = this._configuration.get('prototype.inputLocation') as string;
-        inputLocation = '\"' + inputLocation + '\"';
+        inputLocation = inputLocation;
 
         console.log(inputLocation);
 
@@ -35,7 +60,7 @@ export default class Prototype {
             console.log(value);
         })
 
-        const childProcess = spawn('java', ['-jar', this._prototypePath, ...args], { cwd: this._extensionPath });
+        const childProcess = spawn('java', ['-jar', this._prototypePath, ...args], { cwd: extensionPath });
 
         // Handle events and output from the child process
         childProcess.stdout.on('data', (data) => {
