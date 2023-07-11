69d5f55db26bee6a13f17dc347c46a2945f9b70b
qcp38 <felix.langbein@uni-ulm.de>
2023-07-11 18:33:26 +0200

    written test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-
+import Prototype from '../../prototype';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
@@ -8,8 +8,10 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Get compile file test', () => {
+		const path = 'd:/Felix/Programming/AlDesCo_Git/aldesco_spoo…/pattern_pool/playground/FindAllMethods2.java';
+		
+		const chain = Prototype.getCompiledFromJava(path);
+		console.log(chain)
 	});
 });
